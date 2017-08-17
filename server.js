19992508191d3c13593b11098c8cd90ea1a190cc
comishOmar <comish2009x@live.com>
2017-08-17 20:44:57 +0200

    edited

--- server.js
+++ server.js
@@ -15,7 +15,12 @@ http.createServer(function(req, res){
             res.writeHead(200,{"Content-Type":"text/html"});
             res.end(html);
         });
+    } else if(req.url === "/login"){
 
+        fs.readFile("./login.html","UTF-8",function(err,html){
+            res.writeHead(200,{"Content-Type":"text/html"});
+            res.end(html);
+        });
     } else if (req.url.match(/.css$/)){
 
         var cssPath = path.join(__dirname, req.url);
@@ -31,6 +36,7 @@ http.createServer(function(req, res){
 
         res.writeHead(200, {"Content-Type":"text/ttf"});
         fileStream.pipe(res);
+        
     } else if(req.url.match(/.woff2$/) || (req.url.match(/.woff$/))){
 
         var fpath = path.join(__dirname, req.url);
@@ -38,7 +44,7 @@ http.createServer(function(req, res){
 
         res.writeHead(200, {"Content-Type":"text/ttf"});
         fileStream.pipe(res);
-        
+
     } else if(req.url.match(/.js$/)) {
          var jsPath = path.join(__dirname, req.url);
          var fileStream = fs.createReadStream(jsPath, "UTF-8");
